c94c4aa82e55198012613ce422af4ea4fee14f65
Aaron Ordonez <ordonezaaron67@gmail.com>
2025-01-07 22:54:34 -0500

    just messing with example

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,8 @@
 int main() {
     try {
         // Test parameters
-        int bitDepth = 6; // Bit depth for quantization
-        int sampleRateReductionFactor = 4; // Factor by which the sample rate is reduced
+        int bitDepth = 8; // Bit depth for quantization
+        int sampleRateReductionFactor = 6; // Factor by which the sample rate is reduced
         std::string inputPath = "C:/Users/krono/Desktop/Repos/Bitcrusher/Bitcrusher/test.wav"; // Full path to input
         std::string outputPath = "C:/Users/krono/Desktop/Repos/Bitcrusher/Bitcrusher/output.wav"; // Full path to output
 
